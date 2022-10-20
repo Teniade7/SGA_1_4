25b3aca079e5da3eef41647877bd192eabd71d51
Teniade Adeyemo <teniyemo@gmail.com>
2022-10-20 09:56:31 +0100

    This is my first commit

---
+++ Sga_1_4/week1_day1/hello_world.py
@@ -0,0 +1,6 @@
+print("hello world")
+print("teni's world")
+print('''
+this is going 
+to be the best
+journey of my life''')
---
+++ Sga_1_4/week1_day2/control_flow.py
@@ -0,0 +1,16 @@
+# if statement practice
+x = int(input("please input your grade:"))
+if x >= 70:
+    print("A")
+elif x >= 60 and x <= 69:
+    print("B")
+elif x >= 50 and x <= 59:
+    print("C")
+elif x >= 40 and x <= 49:
+    print("D")
+else:
+    print("failed")
+
+
+
+print("i will execute either way")
